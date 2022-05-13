dc2beaa308b1ec4dc08e4585155a2633ccb0d519
prosper-20 <edwardprosper001@gmail.com>
2022-05-13 04:01:22 +0100

    updates

--- core/models.py
+++ core/models.py
@@ -1,3 +1,51 @@
+from tabnanny import verbose
 from django.db import models
+from PIL import Image
 
-# Create your models here.
+CHOICES = (
+	("1", "Available"),
+	("2", "Not Available")
+)
+
+class Brand(models.Model):
+	name = models.CharField(max_length=255)
+	status = models.CharField(max_length=10, choices=CHOICES)
+
+	def __str__(self):
+		return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=CHOICES)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+	brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+	name = models.CharField(max_length=255)
+	code = models.CharField(max_length=10)
+	image = models.ImageField(upload_to="product_images")
+	quantity = models.IntegerField()
+	rate = models.FloatField(max_length=100)
+	status = models.CharField(max_length=10, choices=CHOICES)
+
+	def __str__(self):
+		return self.name
+
+	def save(self):
+	super().save()
+
+	img = Image.open(self.image.path)
+
+	if img.height > 300 or img.width > 300:
+		output_size = (300, 300)
+		img.thumbnail(output_size)
+		img.save(self.image.path)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
+from typing import List
 from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
+
+# class HomeView(ListView):
+#     model = Item
+#     context_object_name: "items"
+#     template_name = "core/home.html"
