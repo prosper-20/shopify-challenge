39cfe80dc795a230863bc57c410b70a95b14e85a
prosper-20 <edwardprosper001@gmail.com>
2022-05-13 04:19:50 +0100

    updates

--- core/views.py
+++ core/views.py
@@ -1,12 +1,12 @@
 from typing import List
 from django.shortcuts import render
 from django.views.generic import ListView
-# from .models import Product
+from .models import Product
 
 # Create your views here.
 
-# class HomeView(ListView):
-#     model = Product
-#     context_object_name: "products"
-#     template_name = "core/home.html"
+class HomeView(ListView):
+    model = Product
+    context_object_name = "products"
+    template_name = "core/home.html"
     
