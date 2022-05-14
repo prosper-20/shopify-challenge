85987cbc54b85769f3624b741fc142fe756521b9
prosper-20 <edwardprosper001@gmail.com>
2022-05-14 13:15:27 +0100

    users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        passowrd = request.POST['password']
+        password2 = request.POST['password2']
+
+        if passowrd == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "Username already exists!!")
+                return redirect("register")
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, "Email belongs to another user")
+                return redirect("register")
+            else:
+                User.objects.create_user(username=username, email=email, passowrd=password)
+                messages.success(request, f"Hi {username}, your account creation was successful, Kindly login below")
+                return redirect("login")
+        else:
+            messages.error(request, "Both password fields didn't match")
+            return redirect("register")
+    return render(request, "users/register.html")
+            
